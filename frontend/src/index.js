321649da221e9d71390dd6ffa52742c015423ddc
amirgh82 <amirghhastam@gmail.com>
2023-08-02 17:14:33 +0330

    Add images and fonts and styles folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/variables.css'
+import './styles/defaults.css'
+import './styles/fonts.css'
+import './styles/reset.css'
+import './styles/helpers.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
