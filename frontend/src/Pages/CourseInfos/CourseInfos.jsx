7950631e4b1ff9ad00d9b909b93ad3a1f0671c9a
amirgh82 <amirghhastam@gmail.com>
2023-08-02 14:12:55 +0330

    Create folder structure

---
+++ frontend/src/Pages/CourseInfos/CourseInfos.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './CourseInfos.css'
+export default function CourseInfos() {
+    return (
+        <div>CourseInfos</div>
+    )
+}
---
+++ frontend/src/Pages/Index/Index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Index.css'
+export default function Index() {
+  return (
+    <div>Index</div>
+  )
+}
