8b5069e1ee090e53cf7c2357b24a672912c51afb
amirgh82 <amirghhastam@gmail.com>
2023-08-03 15:24:11 +0330

    Add AboutUs and AboutUsBox component

--- frontend/src/Pages/Index/Index.jsx
+++ frontend/src/Pages/Index/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Index.css'
 import Header from '../../Component/Header/Header'
 import LastCourses from '../../Component/LastCourses/LastCourses'
+import AboutUs from '../../Component/AboutUs/AboutUs'
 
 export default function Index() {
   return (
     <div>
       <Header />
       <LastCourses />
+      <AboutUs />
     </div>
   )
 }
