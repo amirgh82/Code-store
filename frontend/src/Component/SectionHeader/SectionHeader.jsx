3d9905ee2f50d6e23d95452c159ee69ee17b11a8
amirgh82 <amirghhastam@gmail.com>
2023-08-10 20:59:49 +0330

    write courses page

--- frontend/src/Component/SectionHeader/SectionHeader.jsx
+++ frontend/src/Component/SectionHeader/SectionHeader.jsx
@@ -1,24 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./SectionHeader.css";
 
-export default function SectionHeader({ title, desc, btnTitle }) {
+export default function SectionHeader({ title, desc, btnTitle, btnHref }) {
+    console.log(`href: ${btnHref}`);
     return (
         <div class="courses-header">
             <div class="courses-header__right">
                 <span class="courses-header__title title">{title}</span>
                 <span class="courses-header__text">{desc}</span>
             </div>
-            {
-                btnTitle ? (
-                    <div class="courses-header__left">
-                        <a href="#" class="courses-header__link">
-                            تمامی دوره ها
-                            <i class="fas fa-arrow-left courses-header__icon"></i>
-                        </a>
-                    </div>
-                ) : null
-            }
+            {btnTitle ? (
+                <div class="courses-header__left">
+                    <Link to={`/${btnHref}`} class="courses-header__link">
+                        {btnTitle}
+                        <i class="fas fa-arrow-left courses-header__icon"></i>
+                    </Link>
+                </div>
+            ) : null}
         </div>
     );
 }
---
+++ frontend/src/Pages/Courses/Courses.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import TopBar from "../../Component/TopBar/TopBar";
+import NavBar from "../../Component/NavBar/NavBar";
+import Breadcrumb from "../../Component/Breadcrumb/Breadcrumb";
+import Footer from "../../Component/Footer/Footer";
+import CourseBox from "../../Component/CourseBox/CourseBox";
+
+import "./Courses.css";
+
+export default function Courses() {
+  return (
+    <>
+      <TopBar />
+      <NavBar />
+
+      <Breadcrumb
+        links={[
+          { id: 1, title: "خانه", to: "" },
+          {
+            id: 2,
+            title: "تمامی دوره ها",
+            to: "courses",
+          },
+        ]}
+      />
+
+      {/* <!--------------------------------  Courses-Section  --------------------------------> */}
+      <section class="courses">
+        <div class="container">
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
+
+          <div class="courses-pagination">
+            <ul class="courses__pagination-list">
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  <i class="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a
+                  href="#"
+                  class="courses__pagination-link courses__pagination-link--active"
+                >
+                  1
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  2
+                </a>
+              </li>
+              <li class="courses__pagination-item">
+                <a href="#" class="courses__pagination-link">
+                  3
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      {/* <!--------------------------------  Courses-Section  --------------------------------> */}
+
+      <Footer />
+    </>
+  );
+}
