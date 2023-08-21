4ddaf18ddd27a8636c7745e8d6540c713af959c5
amirgh82 <amirghhastam@gmail.com>
2023-08-21 20:20:04 +0330

    write reducer in input

--- frontend/src/Component/Form/Input.jsx
+++ frontend/src/Component/Form/Input.jsx
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './Input.css'
 
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE": {
+            return {
+                ...state,
+                value: action.value,
+                isValid: action.isValid
+            }
+        }
+
+            break;
+
+        default: {
+            return state
+        }
+            break;
+    }
+}
 
 export default function Input(props) {
 
     const onChangeHandler = (event) => {
-
+        dispatch({
+            type: 'CHANGE',
+            value: event.target.value,
+            isValid: true
+        })
     }
+
+    const [mainInput, dispatch] = useReducer(inputReducer, {
+        value: '',
+        isValid: false
+    })
 
     const element = props.element === 'input' ? (
         <input
             type={props.type}
             placeholder={props.placeholder}
-            className={props.className}
+            className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
             onChange={onChangeHandler}
+            value={mainInput.value}
         />
     ) : (
         <textarea
             placeholder={props.placeholder}
-            className={props.className}
+            className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
             onChange={onChangeHandler}
+            value={mainInput.value}
         ></textarea>
     )
 
